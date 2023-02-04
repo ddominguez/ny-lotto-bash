877c1ef61a6b9ff306f71348400a06baacb88588
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-02-04 15:15:11 -0500

    output winning numbers

--- lotto.sh
+++ lotto.sh
@@ -14,5 +14,25 @@ fi
 
 game_id=$(echo $games | jq -r ".[] | select(.title == \"$selected_game\") | .nid")
 
-echo "Game ID for $selected_game is $game_id"
+echo "------------------------------"
+echo " $selected_game"
+echo "------------------------------"
+
+case $selected_game in
+    "Mega Millions")
+        bonus_label="Mega Ball"
+        ;;
+    "Powerball")
+        bonus_label="Powerball"
+        ;;
+    *)
+        bonus_label="Bonus Number"
+        ;;
+esac
+
+curl -s -A 'Mozilla/5.0' "https://nylottery.ny.gov/drupal-api/api/v2/winning_numbers?_format=json&nid=$game_id&page=0" \
+    | jq -r ".rows[] |
+        \"Date: \"+.date,
+        \"Winning Numbers: \"+(.winning_numbers | join(\", \")),
+        if (.bonus_number | length) != 0 then \"$bonus_label: \" + .bonus_number + \"\n\" else \"\" end"
 
